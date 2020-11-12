9c2bdca8a1da487726be769a648a8da8e365bf72
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-11-12 20:45:19 -0300

    feat: simple navigating button, using Router

--- web/src/app/views/product-crud/product-crud.component.ts
+++ web/src/app/views/product-crud/product-crud.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-product-crud',
-  templateUrl: './product-crud.component.html',
-  styleUrls: ['./product-crud.component.css']
+  selector: "app-product-crud",
+  templateUrl: "./product-crud.component.html",
+  styleUrls: ["./product-crud.component.css"],
 })
 export class ProductCrudComponent implements OnInit {
+  
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  navigateToCreateProduct(): void {
+    console.info("Redirects to ProductCreate...");
+    this.router.navigate(["/products/create"]);
   }
-
 }
