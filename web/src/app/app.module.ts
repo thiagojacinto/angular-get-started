02a6a417adc3651ac5859f95d126194a618ae716
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-11-16 13:56:37 -0300

    chore: importing Form, FormField & Input modules

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -11,7 +11,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';/*  */
 import { MatCardModule } from '@angular/material/card';/*  */
 import { MatButtonModule } from '@angular/material/button';/*  */
 import { MatSnackBarModule } from '@angular/material/snack-bar';/*  */
-
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
@@ -41,7 +43,10 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
