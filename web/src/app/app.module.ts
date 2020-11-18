efa2ba3a838a3b3e6079802ce398cbf30f975a2f
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-11-18 20:32:42 -0300

    feat: component to handle updating model

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -43,7 +44,8 @@ registerLocaleData(localePt);
     PintarVermelhoDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    MatTemplateTableComponent
+    MatTemplateTableComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
